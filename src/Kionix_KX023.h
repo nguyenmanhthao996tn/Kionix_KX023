ad9f3863ede2aa833748024ff61fed6646c62b00
Manh-Thao NGUYEN <manhthao.spm@gmail.com>
2024-01-17 13:19:41 +0100

    Update: Pulling src/ folder from internal development for Echo Star Terminals

--- src/Kionix_KX023.h
+++ src/Kionix_KX023.h
@@ -4,38 +4,11 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+#include "kx023_data_types.h"
 #include "kx023_reg_map.h"
+#include "kx023_status.h"
 
 #define KX023_DEFAULT_ADDR 0x1E
-
-typedef enum
-{
-	RANGE_2G = 0,
-	RANGE_4G = 1,
-	RANGE_8G = 2,
-} AccelerationRange_t;
-
-typedef enum
-{
-	DATARATE_12_5HZ = 0,
-	DATARATE_25HZ = 1,
-	DATARATE_50HZ = 2,
-	DATARATE_100HZ = 3,
-	DATARATE_200HZ = 4,
-	DATARATE_400HZ = 5,
-	DATARATE_800HZ = 6,
-	DATARATE_1600HZ = 7,
-	DATARATE_0_781HZ = 8,
-	DATARATE_1_563HZ = 9,
-	DATARATE_3_125HZ = 10,
-	DATARATE_6_25HZ = 11,
-} KX023_OutputDatarate_t;
-
-typedef enum
-{
-	LOWPOWER = 0x00,
-	HIGHPOWER = 0x40,
-} KX023_PowerMode_t;
 
 class KX023
 {
@@ -44,27 +17,104 @@ public:
 	KX023(int csPin, SPIClass &spi = SPI);
 	virtual ~KX023();
 
-	int begin(void);
+	KX023_Status_t begin(void);
 	void end(void);
 
-	uint32_t getInterruptStatus(void);
+	KX023_Interrupt_Type_t getInterruptType(void);
 	void clearInterrupt(void);
+	void configPhysicalInterruptPin(int interrupt_number, PhysicalInterruptParameter_t params);
 
-	// Accelerometer
-	int configContinuousReading(KX023_PowerMode_t powerMode = LOWPOWER, AccelerationRange_t accelerationRange = RANGE_2G, KX023_OutputDatarate_t outputDataRate = DATARATE_50HZ); // powerMode 0 = LowPower, 1 = HighPower; accelerationRange 0 = +/-2g, 1 = +/-4g, 2 = +/-8g; outputDataRate = 0(12.5Hz),1(25Hz),2(50Hz),3(100Hz),4(200Hz),5(400Hz),6(800Hz),7(1600Hz),8(0.781Hz),9(1.563Hz),10(3.125Hz),11(6.25Hz)
-	virtual int readAcceleration(float &x, float &y, float &z); // Results are in G (earth gravity).
-	virtual int readAcceleration(float *x, float *y, float *z); // Results are in G (earth gravity).
+	void softwareReset(void);
+	void setStandbyMode(void);
+	void setOperatingMode(void);
 
-	// Single/Double Tap detection
-	int configSingleTapDetection(void);
+	// ==================================
+	// =     NEW DEVELOPING SECTION     =
+	// ==================================
 
-private:
+	// E1. Asynchronous Read Back Acceleration Data
+	KX023_Status_t configAsynchronousReadBackAccelerationData(AccelerationRange_t range, AccelerationOutputDataRate_t odr);
+	KX023_Status_t readAsynchronousReadBackAccelerationData(float *x, float *y, float *z);
+
+	// E2. Synchronous Hardware Interrupt Read Back Acceleration Data
+	KX023_Status_t configSynchronousHardwareInterruptReadBackAccelerationData(AccelerationRange_t range, AccelerationOutputDataRate_t odr);
+
+	// E3. Sample Buffer-Full Interrupt via physical hardware interrupt
+	KX023_Status_t configBufferFullInterrupt(AccelerationRange_t range, AccelerationOutputDataRate_t odr);
+	KX023_Status_t readBuffer(uint8_t *buffer);
+
+	// E4. Wake-Up Function via latched physical hardware interrupt
+	KX023_Status_t configWakeUpFunction(AccelerationRange_t range, MotionWakeUpOutputDataRate_t odr, float desired_delay_time_sec, float desired_threshold, DirectionInfoParams_t direction);
+
+	// E5. Activate Tilt Position Function
+	KX023_Status_t configActivateTiltPosition(AccelerationRange_t range, TiltPositionOutputDataRate_t odr, float desired_delay_time_msec, DirectionInfoParams_t direction);
+
+	// E6. Activate Tap/Double Tap Function
+	KX023_Status_t configActivateTapFunction(AccelerationRange_t range, DirectionalTapOutputDataRate_t odr, DirectionInfoParams_t direction, TapMode_t tap_mode);
+
+protected:
 	int readRegister(uint8_t address);
 	int readRegisters(uint8_t address, uint8_t *data, size_t length);
 	int writeRegister(uint8_t address, uint8_t value);
 
-	void setStandbyMode(void);
-	void setOperatingMode(void);
+	void setPerformanceLowCurrentMode(void);
+	void setPerformanceHighResolutionMode(void);
+
+	void disableNewAccelerationDataInterrupt(void);
+	void enableNewAccelerationDataInterrupt(void);
+
+	void setAccelerationDataRange(AccelerationRange_t range);
+
+	void disableTapFunction(void);
+	void enableTapFunction(void);
+
+	void disableWakeUpFunction(void);
+	void enableWakeUpFunction(void);
+
+	void disableTiltPositionFunction(void);
+	void enableTiltPositionFunction(void);
+
+	void enableFiltering(void);
+	void disableFiltering(void);
+
+	void setFilterCornerFrequency(FilterCornerFrequency_t frequency);
+
+	void setAccelerationDataRate(AccelerationOutputDataRate_t odr);
+
+	void disablePhysicalInterruptPin(int interrupt_number);
+	void enablePhysicalInterruptPin(int interrupt_number, bool interrupt_polarity_active_high, bool interrupt_by_pulse);
+	void configPhysicalInterruptEvent(int interrupt_number, bool buffer_full_interrupt, bool watermark_interrupt, bool data_ready_interrupt, bool tap_function_interrupt, bool wake_up_function_interrupt, bool tilt_position_function_interrupt);
+
+	void disableSampleBuffer(void);
+	void enableSampleBuffer(void);
+
+	void setSampleBufferResolution(bool use_16_bit_res);
+
+	void disableBufferFullInterrupt(void);
+	void enableBufferFullInterrupt(void);
+
+	void setTiltPositionFunctionOutputDataRate(TiltPositionOutputDataRate_t odr);
+	void setDirectionalTapFunctionOutputDataRate(DirectionalTapOutputDataRate_t odr);
+	void setMotionWakeUpFunctionOutputDataRate(MotionWakeUpOutputDataRate_t odr);
+
+	void setMotionWakeUpInterruptDirection(bool x_negative, bool x_positive, bool y_negative, bool y_positive, bool z_negative, bool z_positive);
+	void setInterruptMotionWakeUpTimer(MotionWakeUpOutputDataRate_t odr, float desired_delay_time_sec);
+	void setInterruptMotionWakeUpThreshold(float desired_threshold);
+
+	void setTiltPositionInterruptDirection(bool x_negative, bool x_positive, bool y_negative, bool y_positive, bool z_negative, bool z_positive);
+	void configTiltPositionTimer(TiltPositionOutputDataRate_t odr, float desired_delay_time_msec);
+	void setTiltPositionAngleLowLimit(uint8_t expected_angle);
+	void setTiltPositionAngleHighLimit(uint8_t expected_angle);
+	void setTiltPositionHysteresisAngle(void);
+
+	void setDirectionalTapInterruptDirection(bool x_negative, bool x_positive, bool y_negative, bool y_positive, bool z_negative, bool z_positive);
+	void disableSingleTap(void);
+	void enableSingleTap(void);
+	void disableDoubleTap(void);
+	void enableDoubleTap(void);
+	void setDoubleTapMinimumTimeSeparation(DirectionalTapOutputDataRate_t odr, float desired_minimum_time_separation_between_taps);
+	void setTapHighThresholdValue(uint8_t value);
+	void setTapLowThresholdValue(uint8_t value);
 
 private:
 	TwoWire *_wire;
@@ -74,6 +124,9 @@ private:
 	AccelerationRange_t _accelerationRange;
 
 	SPISettings _spiSettings;
+
+	void setBit(uint8_t register_address, uint8_t bit_index);
+	void clearBit(uint8_t register_address, uint8_t bit_index);
 };
 
 #endif /* __KIONIX_KX023_h__ */
